5ada52b49015e3ec9b4ebd9930a17bb1726d73ba
Silas Aurelius <silasaureliusleone@gmail.com>
2024-10-07 00:35:10 -0400

    Had to rewrite the code for checking temps at 100 or above. I tried using a slice and append but wasn't successful until I added a for loop to iterate over the temperatures list.

--- Slicing-Techniques.py
+++ Slicing-Techniques.py
@@ -3,3 +3,8 @@ temperatures = [72, 75, 78, 79, 80, 81, 82, 83, 85, 86, 87, 88, 89, 90, 91, 92, 
 # Task 1: Extract the temperatures for the second week (7 days) of the month(index 7 to index 14):
 print(temperatures[7:14])
 
+# Had to redo the slice because I had it original set to [8:15]
+
+# Task 2: Extract the temperatures above 100.
+print(len(temperatures))
+#temp_100_and_up = temperatures.append([1])
