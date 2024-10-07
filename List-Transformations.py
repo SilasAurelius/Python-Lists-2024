17a92913eeca897895a4db9f1f93b782b32a99c8
Silas Aurelius <162638735+SilasAurelius@users.noreply.github.com>
2024-10-07 00:52:43 -0400

    Update List-Transformations.py

---
+++ List-Transformations.py
@@ -0,0 +1,9 @@
+# Task 1: Sort the grades in descending order.
+
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+grades.sort()
+grades.reverse()
+print(f"Grades in descending order: {grades}")
+# Task 2: Calculate the average.
+average = sum(grades) / len(grades)
+print(f"The average grade is: {average}")
