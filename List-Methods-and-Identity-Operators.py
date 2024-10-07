f50649c38d518ae1a3968b2c0b0ad7aedfa015b8
Silas Aurelius <silasaureliusleone@gmail.com>
2024-10-07 00:05:00 -0400

    first commit

---
+++ List-Methods-and-Identity-Operators.py
@@ -0,0 +1,7 @@
+# Task 1: Given two lists, find out if Alice submitted their assignment and attended class.
+
+submitted = ["Alice", "Bob", "Charlie", "David"]
+attended = ["Charlie", "Eve", "Alice", "Frank"]
+
+print("Alice" in submitted and "Alice" in attended)
+# 
