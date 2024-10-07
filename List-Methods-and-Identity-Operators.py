a8807f97ad00e868f590d7fb72c44c5e5309dab7
Silas Aurelius <162638735+SilasAurelius@users.noreply.github.com>
2024-10-07 00:53:05 -0400

    Update List-Methods-and-Identity-Operators.py

--- List-Methods-and-Identity-Operators.py
+++ List-Methods-and-Identity-Operators.py
@@ -4,4 +4,5 @@ submitted = ["Alice", "Bob", "Charlie", "David"]
 attended = ["Charlie", "Eve", "Alice", "Frank"]
 
 print("Alice" in submitted and "Alice" in attended)
-# 
+# This did not work at first because I needed to specify to look for "Alice in submitted" and look for "Alice" in attend. Simply used and without typing "Alice" in both conditions of the line would result in a false positive.
+
