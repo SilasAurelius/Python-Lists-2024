06b6271616916b12f08a29a9747fdf4149535ac1
Silas Aurelius <162638735+SilasAurelius@users.noreply.github.com>
2024-10-07 00:49:57 -0400

    Update Slicing-Techniques.py

--- Slicing-Techniques.py
+++ Slicing-Techniques.py
@@ -2,8 +2,7 @@ temperatures = [72, 75, 78, 79, 80, 81, 82, 83, 85, 86, 87, 88, 89, 90, 91, 92, 
 
 # Task 1: Extract the temperatures for the second week (7 days) of the month(index 7 to index 14):
 print(temperatures[7:14])
-
-# Had to redo the slice because I had it original set to [8:15]
+# Had to redo the slice because I had it originally set to [8:15]
 
 # Task 2: Extract the temperatures above 100.
 print(temperatures[23:])
